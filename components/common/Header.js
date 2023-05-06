ac304cb47acb56018e73ba61cd9aca7ebbb4bc01
Ankit Kumar <ankitkr0696@gmail.com>
2023-05-06 13:31:18 +0530

    update home page

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,21 +1,24 @@
+import { COMPANY_NAME } from '@/config';
+import Link from 'next/link';
+
 /* This example requires Tailwind CSS v2.0+ */
 export default function Header() {
     return (
       <div className="bg-sky-700">
         <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-            <span className="block">Boost your productivity.</span>
-            <span className="block">Start using Workflow today.</span>
+            <span className="block">Student Information System</span>
+            <span className="block">{COMPANY_NAME}</span>
           </h2>
           <p className="mt-4 text-lg leading-6 text-sky-200">
-            Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.
+            Welcome to the Student Information System of {COMPANY_NAME}.
           </p>
-          <a
-            href="#"
+          <Link
+            href="/account/login"
             className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-sky-600 bg-white hover:bg-sky-50 sm:w-auto"
           >
-            Sign up for free
-          </a>
+            Log in
+          </Link>
         </div>
       </div>
     )
