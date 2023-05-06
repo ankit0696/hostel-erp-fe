0742211a2663d1f766915e70e3fac1c0a6d688d4
Ankit Kumar <ankitkr0696@gmail.com>
2023-05-06 14:18:56 +0530

    nav link update

--- components/common/Nav.js
+++ components/common/Nav.js
@@ -276,9 +276,9 @@ export default function Nav() {
               </Popover>
             </Popover.Group>
             <div className="flex items-center md:ml-12">
-              <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Sign in
-              </a>
+              </Link>
               <a
                 href="#"
                 className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-sky-600 hover:bg-sky-700"
